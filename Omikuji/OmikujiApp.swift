239501c4eed8d2f639f4aca62a61699b2f67e39e
“taketo45” <taketo45@gmail.com>
2025-01-24 18:15:50 +0900

    Initial Commit

---
+++ Omikuji/OmikujiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmikujiApp.swift
+//  Omikuji
+//
+//  Created by Taketoshi Ikegai on 2025/01/24.
+//
+
+import SwiftUI
+
+@main
+struct OmikujiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
