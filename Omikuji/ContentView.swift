4554712d251b028fac5ccadd3048868896614978
“taketo45” <taketo45@gmail.com>
2025-01-24 19:35:23 +0900

    First Swift App commit

--- Omikuji/ContentView.swift
+++ Omikuji/ContentView.swift
@@ -7,18 +7,48 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    @State var imageName = "omikuji"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(imageName)
+                .resizable()
+                .frame(maxWidth: .infinity)
+            HStack(spacing: 16) {
+                Button("リセット"){
+                    imageName = "omikuji"
+                    
+                }
+                .modifier(ButtonModifier(backgroundColor: .red))
+                Button("おみくじを引く"){
+                    updateOmikujiImageView()
+                    
+                }
+                .modifier(ButtonModifier(backgroundColor: .green))
+            }
         }
-        .padding()
+        .padding(40)
+    }
+    func updateOmikujiImageView(){
+        let lucks = ["daikichi", "kichi", "kyou"]
+        imageName = lucks[Int.random(in: 0...2)]
     }
 }
 
 #Preview {
     ContentView()
 }
+
+struct ButtonModifier: ViewModifier {
+    let backgroundColor: Color
+    func body(content: Content) -> some View {
+        content
+            .frame(maxWidth: .infinity, minHeight: 50)
+            .background(backgroundColor)
+            .foregroundColor(.white)
+    }
+}
+
+
